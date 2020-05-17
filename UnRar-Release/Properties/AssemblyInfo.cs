f3cf3acae2a8762947da65bc6addee7959b862c1
eyebiz <eyebiz@gmail.com>
2020-05-17 20:05:03 +0200

    .NET upgrade and new SharpCompress

--- UnRar-Release/Properties/AssemblyInfo.cs
+++ UnRar-Release/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnRAR-Release")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
